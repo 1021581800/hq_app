a3827d4be437085f22a33ad060a0c22810fd5366
1021581800 <1021581800@qq.com>
2021-03-05 16:36:53 +0800

    111

--- hq_app/settings.py
+++ hq_app/settings.py
@@ -95,7 +95,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'hq_app',
-        'USER': 'hqq',
+        'USER': 'hq',
         'PASSWORD': 'asdf8520',
         'HOST': 'localhost',
         'PORT': '3306',
