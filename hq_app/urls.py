6bbdc993847d8c3f12226c2bc60491c1fafe627e
1021581800 <1021581800@qq.com>
2021-03-09 16:18:18 +0800

    更新

--- hq_app/urls.py
+++ hq_app/urls.py
@@ -26,7 +26,7 @@ urlpatterns = [
     path('ckeditor', include('ckeditor_uploader.urls')),
     path('saygood/',include('saygood.urls')),
 
-    path('user', include('user.urls')),
+    path('user/', include('user.urls')),
 ]
 
 urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
